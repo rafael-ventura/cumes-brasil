d125fdef0f616f2c3ecfe53d66248bd105f26f1b
igordeo-costa <69773445+igordeo-costa@users.noreply.github.com>
2023-08-24 11:02:42 -0300

    Update create_database.sql
    Criação da tabela variantes.

--- database/create_database.sql
+++ database/create_database.sql
@@ -2,9 +2,9 @@
 DROP DATABASE IF EXISTS cumes_brasil;
 CREATE DATABASE cumes_brasil;
 
-use cumes_brasil;
+USE cumes_brasil;
 
--- Verificando e criando a tabela montanhas
+-- Criar a tabela de montanhas
 DROP TABLE IF EXISTS montanhas;
 CREATE TABLE montanhas (
   mount_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -12,13 +12,15 @@ CREATE TABLE montanhas (
   altitude INT
 );
 
+-- Importar tabela csv
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\montanhas.csv'
 INTO TABLE montanhas
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
--- Verificando e criando a tabela faces
+
+-- Criando a tabela de faces
 DROP TABLE IF EXISTS faces;
 CREATE TABLE faces (
   face_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -27,6 +29,7 @@ CREATE TABLE faces (
   FOREIGN KEY (mount_id) REFERENCES montanhas(mount_id)
 );
 
+-- Importar tabela csv
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\faces.csv'
 INTO TABLE faces
 FIELDS TERMINATED BY ','
@@ -34,6 +37,31 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
+-- Criando a tabela com as variantes das vias principais
+DROP TABLE IF EXISTS variantes;
+CREATE TABLE variantes (
+  variante_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  vias VARCHAR(100),
+  grau INT,
+  crux VARCHAR(20),
+  aid VARCHAR(30),
+  duracao VARCHAR(10),
+  exposicao VARCHAR(5),
+  extensao INT,
+  conquistadores VARCHAR(200),
+  data VARCHAR(15),
+  source_id INT
+);
+
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\variantes.csv'
+INTO TABLE variantes
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+-- Criar a tabela  com as fontes primárias
+DROP TABLE IF EXISTS fontes;
 CREATE TABLE fontes (
   source_id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   fontes VARCHAR(200)
@@ -47,7 +75,7 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
--- Verificando e criando a tabela principal
+-- Criando a tabela principal
 DROP TABLE IF EXISTS vias_main;
 CREATE TABLE vias_main (
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
@@ -61,14 +89,16 @@ CREATE TABLE vias_main (
   conquistadores VARCHAR(200),
   data VARCHAR(15),
   mount_id INT,
-  face_id INT NOT NULL,
+  face_id INT,
   source_id INT,
+  variante_id INT,
   FOREIGN KEY (face_id) REFERENCES faces(face_id),
   FOREIGN KEY (mount_id) REFERENCES montanhas(mount_id),
-  FOREIGN KEY (source_id) REFERENCES fontes(source_id)
+  FOREIGN KEY (source_id) REFERENCES fontes(source_id),
+  FOREIGN KEY (variante_id) REFERENCES variantes(variante_id)
 );
 
--- Importando dados para a tabela vias_main
+-- Importar tabela csv para o MySQL
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\vias_principal.csv'
 INTO TABLE vias_main
 FIELDS TERMINATED BY ','
