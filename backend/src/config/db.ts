7108b438fda20fd654365706c30214573c110d1a
vitor <vitor.indio@owse.com.br>
2023-10-29 16:56:37 -0300

    [main-vitor] new Router style + add node to git ignore 1

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -7,3 +7,21 @@ store.initialize();
 // Agora, você pode usar 'store' para interagir com o RavenDB em seu código
 
 export default store;
+
+
+/*
+* 'MySQL' oldWay
+ ----------------------
+* import mysql from 'mysql2/promise';
+
+* const pool = mysql.createPool({
+    host: 'seu_host',
+    user: 'seu_usuario',
+    password: 'sua_senha',
+    database: 'seu_db',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+* });
+export default pool;
+*/
