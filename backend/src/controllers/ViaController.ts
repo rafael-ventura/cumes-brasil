6baa6da98cdbc354c55e0dd0c4002493ca8a875a
vitor <vitor.indio@owse.com.br>
2023-11-02 17:38:27 -0300

    [main-vitor] limpeza codigos inutilizados

--- backend/src/controllers/ViaController.ts
+++ backend/src/controllers/ViaController.ts
@@ -1,28 +1,3 @@
-/*import ViaRepository from '../repositories/ViaRepository';
-import { Request, Response } from 'express';
-
-
-class ViaController {
-    async listAll(req: Request, res: Response) {
-        const vias = await ViaRepository.findAll();
-        res.json(vias);
-    }
-
-    async findById(req: Request , res: Response) {
-        const { id } = req.params;
-        const via = await ViaRepository.findById(Number(id));
-        res.json(via);
-    }
-
-    async findDetailedById(req: Request, res: Response) {
-        const { id } = req.params;
-        const via = await ViaRepository.findDetailedById(Number(id));
-        res.json(via);
-    }
-}
-
-export default new ViaController();*/
-
 import { Request, Response } from 'express';
 import store from '../config/db';
 import { ViaRepository } from '../repositories/ViaRepository';
@@ -51,26 +26,6 @@ class ViaController {
         const via = await this.repo.findDetailedById(id);
         res.json(via);
     }
-
-    //TODO: validar necessidade
-    /*createVia = async (req: Request, res: Response) => {
-        const route: IVia = req.body;
-        const result = await this.repo.create(route);
-        res.json(result);
-    };
-
-    updateVia = async (req: Request, res: Response) => {
-        const id = req.params.id;
-        const updatedRoute: IVia = req.body;
-        const result = await this.repo.update(id, updatedRoute);
-        res.json(result);
-    };
-
-    deleteVia = async (req: Request, res: Response) => {
-        const id = req.params.id;
-        await this.repo.delete(id);
-        res.sendStatus(204);
-    };*/
 }
 
 export default new ViaController();
--- backend/src/routes/ViaRouter.ts
+++ backend/src/routes/ViaRouter.ts
@@ -7,10 +7,4 @@ ViaRouter.get('/:id', ViaController.getViaById);
 ViaRouter.get('/', ViaController.getAllVia);
 ViaRouter.get('/detalhes/:id', ViaController.findDetailedById);
 
-/* TODO: definir rota para criar, editar e deletar vias, se tiver ??:
-    router.post('/', ViaController.createVia);
-    router.put('/vias/:id', ViaController.updateVia);
-    router.delete('/vias/:id', ViaController.deleteVia);
-*/
-
 export default ViaRouter;
