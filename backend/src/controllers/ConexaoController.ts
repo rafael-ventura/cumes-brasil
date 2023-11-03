b377847bef377ff0bdedf145867d9e6c22f3513f
rafael-ventura <raf4ael@gmail.com>
2023-11-02 23:06:47 -0300

    Teste conexao

---
+++ backend/src/controllers/ConexaoController.ts
@@ -0,0 +1,25 @@
+// HealthController.js
+import { Request, Response } from 'express';
+import store from '../config/db';
+
+class ConexaoController {
+    testConnection = async (req: Request, res: Response) => {
+        try {
+            // Tentativa de obter as estatísticas do banco de dados como teste de conexão
+            const session = store.openSession();
+
+            // Se a conexão for bem-sucedida e as estatísticas forem obtidas
+            res.status(200).json({
+                message: "Conexão com o banco de dados RavenDB foi bem-sucedida!"
+            });
+        } catch (error) {
+            // Se houver um erro na conexão com o banco de dados
+            res.status(500).json({
+                message: "Falha ao conectar com o banco de dados RavenDB.",
+                error: error
+            });
+        }
+    };
+}
+
+export default new ConexaoController();
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import ViaRouter from "./ViaRouter";
+import ConexaoController from "../controllers/ConexaoController";
 
 const routes = Router();
 
 routes.use('/vias', ViaRouter);
+routes.get('/conexao', ConexaoController.testConnection);
 
 export default routes;
