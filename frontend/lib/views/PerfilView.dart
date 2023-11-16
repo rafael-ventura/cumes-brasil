f59ca41604483f3f23363841e5d0b8cd28de99cd
elmojuh <junior.fool.skull@gmail.com>
2023-11-16 17:37:23 -0300

    + pagina de perfil - incompleta

--- frontend/lib/views/PerfilView.dart
+++ frontend/lib/views/PerfilView.dart
@@ -4,12 +4,30 @@ class PerfilView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: 100,
-        height: 100,
-        color: Colors.amber,
-        child: Text('Perfil'),
-      ),
-    );
+        appBar: AppBar(
+          titleSpacing: 10,
+          backgroundColor: Colors.amber[800],
+          title: Text(
+            'Meu Perfil',
+            selectionColor: Colors.green,
+          ),
+        ),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Container(
+                width: 300,
+                height: 300,
+                color: Colors.amberAccent,
+                child: Text(
+                  'Dados do usuário',
+                  style: TextStyle(fontSize: 28),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
