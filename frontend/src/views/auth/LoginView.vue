f10ffb15fd27c0831a2a9047e6f9b44f7dd2940f
Vitor Indio <vitor.indio@owse.com.br>
2024-03-01 19:08:52 -0300

    [#46] finalizacao authenticacao + registro do usuario pt1

--- frontend/src/views/auth/LoginView.vue
+++ frontend/src/views/auth/LoginView.vue
@@ -1,47 +1,102 @@
 <template>
   <div>
     <v-img class="mx-auto my-6" max-width="228"
-           src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"></v-img>
+           src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg" />
 
-    <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
-      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+    <v-card class="mx-auto pa-12 pb-8"
+            elevation="8"
+            max-width="448"
+            rounded="lg"
+    >
+      <div class="text-subtitle-1 text-medium-emphasis">Entrar</div>
 
-      <v-text-field density="compact" placeholder="Email address" prepend-inner-icon="mdi-email-outline"
-                    variant="outlined"></v-text-field>
+      <v-text-field
+        density="compact"
+        placeholder="Email"
+        prepend-inner-icon="mdi-email-outline"
+        variant="outlined"
+        @input="maskEmail"
+        v-model="email"
+      >
+      </v-text-field>
 
       <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-        Password
+        Senha
 
-        <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer" target="_blank">
-          Forgot login password?</a>
+        <a
+          class="text-caption text-decoration-none text-blue"
+          href="/reset-password"
+          rel="noopener noreferrer"
+          target="_blank"
+
+        >
+          Esqueci minha senha
+        </a>
       </div>
 
-      <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
-                    density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline"
-                    variant="outlined"
-                    @click:append-inner="visible = !visible"></v-text-field>
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Sua senha"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+        v-model="password"
+      >
+      </v-text-field>
 
-      <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
+      <v-btn
+        block
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+        @click="login"
+        :disabled="!email || !password"
+      >
         Log In
       </v-btn>
 
       <v-card-text class="text-center">
-        <a class="text-blue text-decoration-none" href="#" rel="noopener noreferrer" target="_blank">
-          Sign up now
-          <v-icon icon="mdi-chevron-right"></v-icon>
-        </a>
+        <span class="text-caption mr-2">Não tem conta?</span>
+        <router-link to="/signup" class="text-blue text-decoration-none">Criar conta</router-link>
       </v-card-text>
     </v-card>
     <nav-bar/>
   </div>
 </template>
-<script>
+<script setup>
+import {useRouter} from "vue-router";
 import NavBar from "@/components/NavBar.vue";
+import axios from "axios";
 
-export default {
-  components: { NavBar },
-  data: () => ({
-    visible: false
-  })
-};
+const router = useRouter();
+const visible = false;
+let email = '';
+let password = '';
+
+async function login() {
+  try {
+    const response = await axios.post('http://localhost:3000/login', {
+      email,
+      password
+    });
+    const token = response.data.token;
+    // Armazenar o token no armazenamento local
+    localStorage.setItem('token', token);
+    // Redirecionar para a próxima página
+    await router.push('/dashboard');
+  } catch (error) {
+    //TODO: notify pro usuário
+    console.error('Login failed:', error);
+  }
+}
+
+// Máscara de email
+async function maskEmail(event) {
+  // Lógica para mascarar o campo de email
+  const emailInput = event.target.value;
+  email = emailInput.replace(/./g, '*');
+}
 </script>
