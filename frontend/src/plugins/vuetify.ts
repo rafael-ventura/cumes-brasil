65b2aba386371ae06e7e884a9f6c632d72dc9c28
elmojuh <junior.fool.skull@gmail.com>
2024-03-30 04:18:29 -0300

    esqueci de voltar com isso

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -2,15 +2,17 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import "@mdi/font/css/materialdesignicons.css";
-import { VBtn } from 'vuetify/components/VBtn';
+import { VBtn } from 'vuetify/components';
 import "@/assets/styles.scss";
 
 const vuetify = createVuetify({
   components,
   directives,
-  defaults: {
-    VBtn: {
-    }
+  icons: {
+    defaultSet: "mdi"
+  },
+  theme: {
+    defaultTheme: "dark"
   }
 });
 
