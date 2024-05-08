ccaa039f2d42398d2dd981bdd5908cf453f3a830
rafael-ventura <raf4ael@gmail.com>
2024-05-08 18:30:34 -0300

    Criacao de script inicial de carga de dados

--- backend/Infrastructure/sql_scripts/initialLoad.ts
+++ backend/Infrastructure/sql_scripts/initialLoad.ts
@@ -1,120 +1,45 @@
-import { FonteService } from "../../Application/services/FonteService";
-import { FonteRepository } from "../repositories/FonteRepository";
-import { Face } from "../../Domain/entities/Face";
-import { FaceRepository } from "../repositories/FaceRepository";
-import { FaceService } from "../../Application/services/FaceService";
-import { MontanhaRepository } from "../repositories/MontanhaRepository";
-import { MontanhaService } from "../../Application/services/MontanhaService";
+import { AppDataSource } from "../config/db";
 import { Fonte } from "../../Domain/entities/Fonte";
-import { Usuario } from "../../Domain/entities/Usuario";
-import { Colecao } from "../../Domain/entities/Colecao";
-import { Croqui } from "../../Domain/entities/Croqui";
 import { Montanha } from "../../Domain/entities/Montanha";
-import usuarioService from "../../Application/services/UsuarioService";
-import { ColecaoRepository } from "../repositories/ColecaoRepository";
-import { ColecaoService } from "../../Application/services/ColecaoService";
-import { EscaladaRepository } from "../repositories/EscaladaRepository";
-import { EscaladaService } from "../../Application/services/EscaladaService";
-import { CroquiRepository } from "../repositories/CroquiRepository";
-import { CroquiService } from "../../Application/services/CroquiService";
-import { ViaRepository } from "../repositories/ViaRepository";
-import { ViaService } from "../../Application/services/ViaService";
+import { Face } from "../../Domain/entities/Face";
+import { Via } from "../../Domain/entities/Via";
+import { Croqui } from "../../Domain/entities/Croqui";
+import * as initialData from "../../../database/json/initialData.json";
 
-export const initialLoad = async () => {
-  const fonteService = new FonteService(new FonteRepository());
-  const montanhaService = new MontanhaService(new MontanhaRepository());
-  const faceService = new FaceService(new FaceRepository(), fonteService, montanhaService);
-  const viaService = new ViaService(new ViaRepository());
-  const croquiService = new CroquiService(new CroquiRepository(), new ViaRepository());
-  const escaladaService = new EscaladaService(new EscaladaRepository(), usuarioService, viaService);
-  const colecaoService = new ColecaoService(new ColecaoRepository(), viaService, usuarioService);
+export async function loadData () {
+  const queryRunner = AppDataSource.createQueryRunner();
 
-  const data = {
-    usuarios: [
-      {
-        "nome": "Carlos Caralinho",
-        "email": "carlos@gmail.com.br",
-        "password_hash": "123456",
-        "fotoPerfil": "https://google.com"
-      } as Usuario
-    ],
-    fontes: [
-      {
-        "autor": "ElmoJuh Boquete Fofo",
-        "referencia": "Elmo Juh com sua revista ficticia"
-      } as Fonte
-    ],
-    montanhas: [
-      {
-        "nome": "Morro Pica de Acucar",
-        "localizacao": "Rio de Janeiro",
-        "altura": 256,
-        "fonte_id": 1
-      } as Montanha
-    ],
-    faces: [
-      {
-        "nome": "Face Cequelete",
-        "montanha_id": 1,
-        "fonte_id": 1
-      } as Face
-    ],
-    /*  vias: [
-       {
-         "nome": "Desafio do Pico",
-         "grau": "VIsup",
-         "crux": "Passagem técnica no meio da via",
-         "artificial": "A2",
-         "duracao": "D3",
-         "exposicao": "E2",
-         "extensao": 150.6,
-         "conquistadores": "['Ana Silva', 'Carlos Rocha']",
-         "detalhes": "Via de grande beleza cênica, com trechos de escalada livre e artificial. Exige boa técnica e resistência.",
-         "data": "2021-07-15",
-         "montanha_id": 1,
-         "via_principal_id": null,
-         "fonte_id": 1,
-         "face_id": 1
-       } as Via
-     ], */
-    croquis: [
-      {
-        "nome": "croqui de merda",
-        "imagemUrl": "https://google.com",
-        "autor": "Autor Filha da Puta",
-        "descricao": "Croqui.",
-        "fonte_id": 1
-      } as Croqui
-    ],
-    /* escaladas: [
-      {
-        "nome": "Escalada do ovo esquerdo e direito",
-        "data": "2021-07-15",
-        "observacao": "Caguei 3 vezes na via",
-        "usuario_id": 1,
-        "via_id": 1
-      } as Escalada
-    ], */
-    colecoes: [
-      {
-        "nome": "Colecao 3",
-        "descricao": "col 3",
-        "usuario_id": 1
-      } as Colecao
-    ]
-  };
+  await queryRunner.connect();
+  await queryRunner.startTransaction();
 
-// Usuario, Fonte, Montanha, Face, Via, Croqui, Escalada, Colecao
-  usuarioService.register(data.usuarios[0].nome, data.usuarios[0].email, data.usuarios[0].password_hash);
-  fonteService.createFonte(data.fontes[0]);
-  montanhaService.createMontanha(data.montanhas[0]);
-  faceService.createFace(data.faces[0]);
-  /* viaService.createVia(data.vias[0]); */
-  croquiService.createCroqui(data.croquis[0]);
-  /* escaladaService.createEscalada(data.escaladas[0]); */
-  colecaoService.createColecao(data.colecoes[0]);
-};
+  try {
+    const fonteRepository = queryRunner.manager.getRepository(Fonte);
+    const montanhaRepository = queryRunner.manager.getRepository(Montanha);
+    const faceRepository = queryRunner.manager.getRepository(Face);
+    const viaRepository = queryRunner.manager.getRepository(Via);
+    const croquiRepository = queryRunner.manager.getRepository(Croqui);
 
+    // Processo de criação e salvamento das entidades
+    const fontes = fonteRepository.create(initialData.fontes);
+    await fonteRepository.save(fontes);
 
+    const montanhas = montanhaRepository.create(initialData.montanhas);
+    await montanhaRepository.save(montanhas);
 
+    const faces = faceRepository.create(initialData.faces);
+    await faceRepository.save(faces);
 
+    const vias = viaRepository.create(initialData.vias);
+    await viaRepository.save(vias);
+
+    const croquis = croquiRepository.create(initialData.croquis);
+    await croquiRepository.save(croquis);
+
+    await queryRunner.commitTransaction();
+  } catch (error) {
+    console.error("Erro ao carregar dados:", error);
+    await queryRunner.rollbackTransaction();
+  } finally {
+    await queryRunner.release();
+  }
+}
