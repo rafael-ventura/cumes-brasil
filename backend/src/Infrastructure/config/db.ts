aa3850f3466bf6a1316feb1b8364caf97d2f6186
Rafael Ventura <raf4ael@gmail.com>
2024-09-10 00:05:59 -0300

    Nao me garanto tanto

--- backend/src/Infrastructure/config/db.ts
+++ backend/src/Infrastructure/config/db.ts
@@ -15,7 +15,11 @@ initializeEnvConfig();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    url: process.env.DATABASE_URL,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT || '5432', 10),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [
