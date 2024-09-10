64a53c882ec7b172b10081ecdd3d17df5ecb4f10
Rafael Ventura <raf4ael@gmail.com>
2024-09-09 23:50:33 -0300

    Ajustes .env para bater com producao

--- backend/src/Infrastructure/config/db.ts
+++ backend/src/Infrastructure/config/db.ts
@@ -15,11 +15,7 @@ initializeEnvConfig();
 
 export const AppDataSource = new DataSource({
     type: 'postgres',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    url: process.env.DATABASE_URL,
     synchronize: true,
     logging: false,
     entities: [
