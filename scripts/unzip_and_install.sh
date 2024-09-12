c926c810ab8d641ff1aa47f425d20dbb3c09f917
Rafael Ventura <raf4ael@gmail.com>
2024-09-12 16:11:05 -0300

    Ajuste do pipeline do back

--- scripts/unzip_and_install.sh
+++ scripts/unzip_and_install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Define o caminho da pasta onde o CodeDeploy baixa os arquivos
+DEPLOY_DIR=/opt/codedeploy-agent/deployment-root/deployment-archive
+
 # Descompacta o arquivo backend.zip
 echo "Descompactando backend.zip..."
-unzip /home/ec2-user/cumes-backend/backend.zip -d /home/ec2-user/cumes-backend
+unzip "$DEPLOY_DIR/backend.zip" -d /home/ec2-user/cumes-backend
+
+# Verifica se a descompactação foi bem-sucedida
+if [ $? -ne 0 ]; then
+    echo "Falha ao descompactar o backend.zip"
+    exit 1
+fi
 
 # Navega até o diretório backend
 cd /home/ec2-user/cumes-backend/backend || exit 1
