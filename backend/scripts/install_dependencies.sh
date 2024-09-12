3892836c86a7582b8c0cc1b3ea89532762b62329
Rafael Ventura <raf4ael@gmail.com>
2024-09-12 03:36:47 -0300

    Ajuste do pipeline do back

--- backend/scripts/install_dependencies.sh
+++ backend/scripts/install_dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Navega até o diretório do projeto
-cd /home/ec2-user/cumes-backend
-
 # Atualiza o sistema e instala Node.js e npm usando yum
 sudo yum update -y
 sudo yum install -y nodejs npm
+
+# Navega até o diretório do projeto
+cd /home/ec2-user/cumes-backend/backend
 
 # Instala as dependências do projeto
 npm install
