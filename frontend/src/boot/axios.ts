94eef3a9a150a66199ed715cda44d0ced9bc4b22
Rafael Ventura <raf4ael@gmail.com>
2024-10-21 19:46:03 -0300

    [#producao RAFA] - v01 - fix 0.5

--- frontend/src/boot/axios.ts
+++ frontend/src/boot/axios.ts
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers';
 import axios from 'axios';
 
-const api = axios.create({ baseURL: import.meta.env.VITE_APP_API_URL});
+const api = axios.create({ baseURL: import.meta.env.VITE_APP_API_URL });
 api.interceptors.response.use(
   response => response,
   error => {
