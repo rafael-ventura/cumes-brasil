2d7f93b4459bbf8ebd91f1d299a215265d120d02
Rafael Ventura <raf4ael@gmail.com>
2024-11-11 19:51:31 -0300

    [#main] - Ajuste card colecao

--- frontend/src/components/Colecao/ColecaoCard.vue
+++ frontend/src/components/Colecao/ColecaoCard.vue
@@ -1,15 +1,26 @@
 <template>
   <q-card class="colecao-card" @click="emitClick" v-if="colecao && colecao.id">
-    <q-card-section>
+    <q-card-section class="colecao-content">
       <div class="colecao-image">
-        <q-img :src="colecao.imagem?.url || 'https://via.placeholder.com/300x150'" alt="Imagem da Coleção" />
+        <!-- Exibe o `q-img` se `colecao.imagem?.url` estiver disponível -->
+        <q-img
+          v-if="colecao.imagem?.url"
+          :src="colecao.imagem?.url"
+          alt="Imagem da Coleção"
+        />
+        <!-- Caso contrário, exibe o componente `ImagePlaceholder` -->
+        <ImagePlaceholder v-else :fillColor="'#fcbd7b'" />
       </div>
       <div class="colecao-info">
         <div class="text-h6">{{ colecao.nome }}</div>
         <div class="text-subtitle1">{{ colecao.descricao }}</div>
 
         <!-- Exibe o número de vias apenas se `viasCarregadas` tiver sido preenchido -->
-        <q-badge v-if="viasCarregadas !== null" color="primary" :label="'Vias na coleção: ' + viasCarregadas" />
+        <q-badge
+          v-if="viasCarregadas !== null"
+          color="primary"
+          :label="'Vias na coleção: ' + viasCarregadas"
+        />
         <q-badge v-else color="grey" label="Carregando vias..." />
       </div>
     </q-card-section>
@@ -17,38 +28,30 @@
 </template>
 
 <script setup lang="ts">
-import { defineProps, defineEmits, ref, watch, onMounted } from 'vue';
+import { defineEmits, defineProps, onMounted, ref, watch } from 'vue';
 import { IColecao } from 'src/models/IColecao';
 import { useRouter } from 'vue-router';
+import ImagePlaceholder from 'components/ImagePlaceholder.vue';
 
 const router = useRouter();
 const props = defineProps<{ colecao: IColecao }>();
 const emits = defineEmits(['click']);
 const viasCarregadas = ref<number | null>(null); // Começa como null para mostrar "Carregando"
 
-// Função para verificar e atualizar `viasCarregadas` quando `colecao.vias` estiver disponível
 const verificarVias = () => {
   if (props.colecao.viaColecoes) {
-    console.log('entrei', props.colecao.viaColecoes.length);
     viasCarregadas.value = props.colecao.viaColecoes.length;
   } else {
-    // esperar 3 segundos e veriiicar novamente
     setTimeout(() => {
-      console.log('entrei else', props.colecao.viaColecoes?.length);
       verificarVias();
     }, 3000);
   }
 };
 
-// Observa mudanças em `colecao.vias`
 watch(
   () => props.colecao.viaColecoes?.via,
   (vias) => {
-    if (vias) {
-      viasCarregadas.value = vias.length;
-    } else {
-      viasCarregadas.value = null;
-    }
+    viasCarregadas.value = vias ? vias.length : null;
   },
   { immediate: true }
 );
@@ -58,36 +61,32 @@ const emitClick = () => {
   router.push(`/colecoes/${props.colecao.id}`);
 };
 
-// Chamada inicial para verificar as `vias`
 onMounted(() => {
   verificarVias();
 });
 </script>
+
 <style scoped lang="scss">
 @import "src/css/app.scss";
 
 .colecao-card {
   width: 100%;
   max-width: 892px;
-  height: 132px;
   margin: auto;
-  display: flex;
-  align-items: center;
-  background-color: #fcbd7b;
-  border-radius: 12px;
   cursor: pointer;
   transition: transform 0.2s;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 12px;
+  background-color: #fcbd7b;
 
   &:hover {
     transform: translateY(-4px);
   }
 }
 
-.card-content {
+.colecao-content {
   display: flex;
   align-items: center;
-  padding: 8px;
 }
 
 .colecao-image {
@@ -95,11 +94,12 @@ onMounted(() => {
   height: 96px;
   border-radius: 8px;
   overflow: hidden;
-  margin-right: 12px;
+  margin-right: 16px;
   display: flex;
   align-items: center;
   justify-content: center;
   background-color: #e0e0e0;
+  flex-shrink: 0; /* Impede a imagem de redimensionar */
 }
 
 .colecao-info {
@@ -107,5 +107,16 @@ onMounted(() => {
   flex-direction: column;
   justify-content: center;
   color: #2c2c2c;
+  flex-grow: 1; /* Garante que a informação ocupe o restante do espaço */
+}
+
+.text-h6 {
+  font-weight: bold;
+  margin-bottom: 4px;
+}
+
+.text-subtitle1 {
+  font-size: 0.9em;
+  margin-bottom: 8px;
 }
 </style>
