89325e7a57376166bf2bcf85abcaefc857626a5f
Rafael Ventura <raf4ael@gmail.com>
2024-12-17 19:57:38 -0300

    :bricks: :wrench: Ajustando todo o fluxo de pipeline

--- scripts/stop_services.sh
+++ scripts/stop_services.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 echo "Parando e limpando serviços antigos..."
 
-# Remover o diretório node_modules para evitar conflitos
-rm -rf /home/ec2-user/cumes-brasil/backend/node_modules
+# Navegar até o diretório do projeto
+cd /home/ec2-user/cumes-brasil || exit 1
 
 # Parar e remover todos os contêineres em execução
 docker-compose down
 
-# Parar e remover todos os contêineres (ativos ou parados)
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
+# Parar e remover todos os contêineres (ativos ou parados) - não necessário se já rodamos docker-compose down, mas mantemos por segurança
+docker stop $(docker ps -aq) || true
+docker rm $(docker ps -aq) || true
 
-# Remover todos os volumes
-docker volume rm $(docker volume ls -q)
+# NÃO REMOVER VOLUMES E IMAGENS!
+# Assim mantemos o banco de dados e dados persistentes intactos.
+# docker volume rm $(docker volume ls -q)
+# docker rmi $(docker images -q) -f
 
-# Remover todas as imagens
-docker rmi $(docker images -q) -f
-
-echo "Serviços antigos parados e limpos com sucesso!"
+echo "Serviços antigos parados. Volumes e dados preservados."
