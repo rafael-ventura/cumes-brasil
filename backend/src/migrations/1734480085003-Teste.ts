781bd5148fa499045fb5d7a17964f21e32fc20df
Rafael Ventura <raf4ael@gmail.com>
2024-12-17 21:10:29 -0300

    :bricks: :wrench: Teste Migrations + Pipeline

---
+++ backend/src/migrations/1734480085003-Teste.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Teste1734480085003 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
