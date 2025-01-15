186252bf8bcf5f7cf434f4ad7eed42070237eb1d
Rafael Ventura <raf4ael@gmail.com>
2025-01-14 21:25:11 -0300

    add run manual

--- scripts/stop_services.sh
+++ scripts/stop_services.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo "Parando e limpando serviços antigos..."
 
-cd /home/ec2-user/cumes-brasil || exit 1
-
-docker-compose down
-
-docker stop $(docker ps -aq) || true
-docker rm $(docker ps -aq) || true
+#cd /home/ec2-user/cumes-brasil || exit 1
+#
+#docker-compose down
+#
+#docker stop $(docker ps -aq) || true
+#docker rm $(docker ps -aq) || true
 
 echo "Serviços antigos parados. Volumes e dados preservados."
